b463ef0abb34717f982d31073e3f2b172fa19974
Jason Sutter <jasonsutter87@gmail.com>
2022-01-27 11:19:10 -0800

    testing new js

--- netlify/functions/newRow.js
+++ netlify/functions/newRow.js
@@ -1,14 +1,32 @@
-const JSONDATA  = {{ $.Site.Data.priceAndHash.data }}
+setTimeout(() => {
+    $(() =>{
+      //ready
+      console.log('data is loading.')
+      const JSONDATA  = {{ $.Site.Data.priceAndHash.data }}
 
-$.ajax({
-  url: 'https://api.blockchain.info/stats',
-  success: data => {
+      $.ajax({
+        url: 'https://api.blockchain.info/stats',
+        success: data => {
 
-    JSONDATA.push({
-      price: data.market_price_usd,
-      hash_rate: data.hash_rate
-    })
+          JSONDATA.push({
+            price: data.market_price_usd,
+            hash_rate: data.hash_rate
+          })
 
-    console.log(JSONDATA)
-  }
-})
+          console.log(JSONDATA)
+
+          $('#price-and-hash-content').append(`
+          <div class="row" >
+            <div class="col-2">
+              <p>${data.market_price_usd}</p>
+            </div>
+            <div class="col">
+              <p>${data.hash_rate}</p>
+            </div>
+          </div>`
+          )
+        }
+      })
+
+    } )
+  }, 500)
